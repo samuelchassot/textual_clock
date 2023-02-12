1baee2c24ba1c710b5002dcd9e74c940bb447c42
Samuel Chassot <samuel.chassot@epfl.ch>
2023-02-12 13:22:43 +0100

    w

--- web_server.py
+++ web_server.py
@@ -50,7 +50,7 @@ def store_color(color_tuple: tuple[int, int, int]) -> None:
 
 
 def read_current_color() -> tuple[int, int, int]:
-    with open(CURRENT_COLOR_FILE_PATH, "r") as f:
+    with open(CURRENT_COLOR_FILE_PATH, "ra") as f:
         try:
             l = f.readline()
             print("Read color line: " + l)
