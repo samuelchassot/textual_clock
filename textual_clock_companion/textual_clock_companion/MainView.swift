4e908db88570980a29225b4d47168ec9b5c3ebf1
Samuel Chassot <samuel.chassot@epfl.ch>
2023-02-12 13:18:37 +0100

    ios app

--- textual_clock_companion/textual_clock_companion/MainView.swift
+++ textual_clock_companion/textual_clock_companion/MainView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MainView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
-    @State private var bgColor =
+    @State private var selectedColor =
     Color(.sRGB, red: 0.98, green: 0.9, blue: 0.2)
     
     @State private var clockAddress = "raspberry"
@@ -25,12 +25,12 @@ struct MainView: View {
                 Spacer()
                 Text("Select a color for the clock's text:")
                     .font(.headline)
-                ColorPicker("Clock color", selection: $bgColor)
+                ColorPicker("Clock color", selection: $selectedColor)
                     .scaleEffect(CGSize(width: 2, height: 2))
                     .labelsHidden()
                 
                     .padding([.top, .leading, .trailing])
-                    
+                
                 Spacer()
                 Button(action: reboot){
                     Text("Apply color")
@@ -63,6 +63,21 @@ struct MainView: View {
     }
     
     private func updateColor(color: Color){
+        let arrRgb = UIColor(self.selectedColor).cgColor.components!
+        let rgbColor = RgbColor(color_r: arrRgb[0], color_g: arrRgb[1], color_b: arrRgb[2])
+        HttpClockApiUtility.sendColorUpdate(clockAddress: self.clockAddress, color: rgbColor, onSuccess: {(msg) in
+            // TODO
+        }, onError: {(errorMsg) in
+            // TODO
+        })
+    }
+    
+    private func getCurrentClockColor(){
+        HttpClockApiUtility.getCurrentColor(clockAddress: self.clockAddress, onSuccess: {(rgbColor) in
+            self.selectedColor = Color(red: rgbColor.color_r, green: rgbColor.color_g, blue: rgbColor.color_b)
+        }, onError: {(errorMsg) in
+            self.selectedColor = Color(.sRGB, red: 0.98, green: 0.9, blue: 0.2)
+        })
     }
     
     private func loadClockName(){
