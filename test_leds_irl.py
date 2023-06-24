b9466216696f0247e00dcbe463d13ef2ce7a68ae
Samuel Chassot <samuel.chassot@epfl.ch>
2023-06-24 16:45:19 +0200

    color

--- test_leds_irl.py
+++ test_leds_irl.py
@@ -2,6 +2,10 @@ import board
 import neopixel
 import time
 
+CURRENT_COLOR_FILE_PATH = "res/color.current"
+SEPARATOR = ";"
+DEFAULT_COLOR = (255, 255, 255)
+
 
 def main():
     n_leds_per_line = 11
@@ -12,5 +16,16 @@ def main():
         pixels.fill((0, 0, 0))
         time.sleep(2)
         for i in range(n_leds):
-            pixels[i] = (0, 255, 0)
+            pixels[i] = read_current_color()
             time.sleep(0.8)
+
+
+def read_current_color() -> tuple[int, int, int]:
+    try:
+        with open(CURRENT_COLOR_FILE_PATH, "r") as f:
+            l = f.readline()
+            rgb = l.split(SEPARATOR)
+            return (int(rgb[0]), int(rgb[1]), int(rgb[2]))
+    except Exception as e:
+        print("ERROR: cannot read the current color!\n", e)
+        return DEFAULT_COLOR
