ef772e799f573d190493dec4281cc16985e59c4b
Samuel Chassot <samuel.chassot@epfl.ch>
2023-06-24 14:14:42 +0200

    new script that waits on the internet

--- start.sh
+++ start.sh
@@ -1,6 +1,19 @@
 # DO NOT REMOVE, IT IS THE ENTRY POINT FOR THE CLOCK
 cd /home/chsa/textual_clock/
-sleep 45
+
+# Check for internet connection
+while true; do
+  if ping -c 1 google.com; then
+    echo "Internet is up. Proceeding..."
+    break
+  else
+    echo "Waiting for internet connection..."
+    # Sleep for 5 seconds before checking again
+    sleep 5
+  fi
+done
+
+# Your actual script starts here
 git pull
 sudo pip3 install -r requirements.txt
 # sudo python3 clock_app.py
