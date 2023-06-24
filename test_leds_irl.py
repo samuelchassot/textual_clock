4020cde24e44d261cccf9aa31e81e9d17fb7bf44
Samuel Chassot <samuel.chassot@epfl.ch>
2023-06-24 13:50:19 +0200

    keep leds on

--- test_leds_irl.py
+++ test_leds_irl.py
@@ -8,7 +8,7 @@ n_leds = n_leds_per_line * 10
 pixels = neopixel.NeoPixel(board.D18, n_leds)
 
 while True:
+    pixels.fill((0, 0, 0))
     for i in range(n_leds):
-        pixels.fill((0, 0, 0))
         pixels[i] = (255, 255, 255)
         time.sleep(0.1)
