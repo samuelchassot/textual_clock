0c0b2cf9476ec84610c89b597f960cdfe104be0c
Samuel Chassot <samuel.chassot@epfl.ch>
2026-03-21 15:40:35 +0100

    remove testing

--- clock_app.py
+++ clock_app.py
@@ -79,11 +79,9 @@ def read_current_color() -> tuple[int, int, int]:
 
 
 if __name__ == "__main__":
-    # n_leds_per_line = 11
-    # n_leds = n_leds_per_line * 10
-    # pixels = neopixel.NeoPixel(board.D18, n_leds)
-    # clk = clock.Clock(n_leds_per_line, pixels)
-    # clk.run()
-    th = threading.Thread(target=test_leds_irl.main)
-    th.start()
+    n_leds_per_line = 11
+    n_leds = n_leds_per_line * 10
+    pixels = neopixel.NeoPixel(board.D18, n_leds)
+    clk = clock.Clock(n_leds_per_line, pixels)
+    clk.run()
     app.run(host=HOST, port=PORT)
