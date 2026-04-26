faddd9488982d1bf6ae7d133ca2004a9209e4266
Samuel Chassot <samuel.chassot@epfl.ch>
2026-04-26 15:59:57 +0200

    install more

--- raspberry_install.sh
+++ raspberry_install.sh
@@ -2,4 +2,5 @@ sudo apt-get update
 sudo apt-get install python3-pip
 sudo apt-get install python3-venv
 sudo apt install python3-rpi-lgpio python3-lgpio
+sudo apt install swig build-essential python3-dev
 sudo apt install git
