ce65e1a4faec8fa387cb4e0ce7e5438e56257eb3
Samuel Chassot <14821693+samuelchassot@users.noreply.github.com>
2026-04-26 15:31:05 +0200

    Add python3-dev and python3-rpi.gpio installation

--- raspberry_install.sh
+++ raspberry_install.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install python3-pip
 sudo apt-get install python3-venv
+sudo apt-get install python3-dev python3-rpi.gpio
 sudo apt install git
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
