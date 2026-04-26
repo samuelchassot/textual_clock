2f0b6bed693faa2c79d999a9e67b260af4bea903
Samuel Chassot <samuel.chassot@epfl.ch>
2026-04-26 19:05:38 +0200

    script

--- raspberry_install.sh
+++ raspberry_install.sh
@@ -1,3 +1,5 @@
+# Execute these commands on the Raspberry Pi's terminal
+
 sudo apt update
 sudo apt install -y \
 python3-venv python3-pip python3-dev \
@@ -9,6 +11,8 @@ git clone https://github.com/samuelchassot/textual_clock.git
 cd ~/textual_clock
 
 python3 -m venv venv --system-site-packages
+
+source venv/bin/activate
 
 pip install \
 rpi_ws281x==5.0.0 \
